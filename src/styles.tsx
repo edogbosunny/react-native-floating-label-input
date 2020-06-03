d739fd4c3e1f69c81326f88ef404e86194e38fb2
Edogbo Sunny <sunnyedogbo@gmail.com>
2020-06-04 00:06:49 +0100

    Update styles.tsx

--- src/styles.tsx
+++ src/styles.tsx
@@ -3,9 +3,10 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   input: {
     paddingVertical: 0,
-    paddingHorizontal: 30,
+    paddingHorizontal: 20,
     flex: 1,
     zIndex: 1,
+    color: '#344356'
   },
   img: {
     width: 20,
